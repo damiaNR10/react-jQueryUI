e706f17fe7615ffa60a700f7e957ea669d465f5c
damiaNR10 <damianrog96@gmail.com>
2021-03-27 22:15:51 +0100

    MomentJS installed

--- src/App.js
+++ src/App.js
@@ -1,46 +1,74 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom";
 import './App.css';
 import React from 'react';
 
-class DatePicker extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.datePickerContainer = React.createRef();
-  }
-
-  componentDidMount() {
-    window.$(this.datePickerContainer.current).datepicker({
-      onSelect: this.props.onDateChanged,
-    });
-  }
-
-  componentWillUnmount() {
-    window.$(this.datePickerContainer.current).datepicker("destroy");
-  }
-
-  render() {
-    return (
-      <div ref = {this.datePickerContainer}/>
-    )
-  }
-}
-
 class App extends React.Component {
-
   state = {
-    selectedDate: null,
-  }
-
+    selectedDate: "09/22/2019"
+  };
   render() {
     return (
       <div className="App">
-        <h1>Hello jQueryUI!</h1>
-        <h2>{this.state.selectedDate ? this.state.selectedDate : "Please select date"}</h2>
-        <DatePicker onDateChanged={date => {this.setState({selectedDate: date})}}/>
+        <h1>Hello Moment.js</h1>
+        <h2>
+          {this.state.selectedDate
+            ? this.state.selectedDate
+            : "Please select date"}
+        </h2>
+        <DatePicker
+          initialDate={this.state.selectedDate}
+          onDateChange={date => this.setState({ selectedDate: date })}
+        />
+        {this.state.selectedDate ? (
+          <DateDetails date={this.state.selectedDate} format="MM/DD/YYYY" />
+        ) : null}
       </div>
     );
   }
 }
+
+function DateDetails({ date, format }) {
+
+  const theDate = new Date(date);
+  const now = new Date();
+
+  return (
+    <div className="DateDetails">
+      <h2>Fun facts about this date</h2>
+      <ol>
+        <li>The date is: {theDate.toString()}</li>
+        <li>
+          Counting from now ({now.toDateString()}), it would be {"6 years ago"}.
+        </li>
+        <li>
+          Next valentine's day ({"XXXX"}) will be {"in 3 weeks"}
+        </li>
+        <li>It does not fall within a leap year.</li>
+        <li>
+          It is a summer day (it's between {"XXXX"} and {"YYYY"}).
+        </li>
+        <li>It is not the Programmer's Day ({"XXXX"}).</li>
+      </ol>
+    </div>
+  );
+}
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.datepickerContainer = React.createRef();
+  }
+  componentDidMount() {
+    window.$(this.datepickerContainer.current).datepicker({
+      onSelect: this.props.onDateChange,
+      defaultDate: this.props.initialDate
+    });
+  }
+
+  render() {
+    return <div ref={this.datepickerContainer} />;
+  }
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
