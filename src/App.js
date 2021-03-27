15f8e8781674121a3adbb730341f817de7586c64
damiaNR10 <damianrog96@gmail.com>
2021-03-27 19:00:33 +0100

    FInal commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DatePicker extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.datePickerContainer = React.createRef();
+  }
+
+  componentDidMount() {
+    window.$(this.datePickerContainer.current).datepicker({
+      onSelect: this.props.onDateChanged,
+    });
+  }
+
+  componentWillUnmount() {
+    window.$(this.datePickerContainer.current).datepicker("destroy");
+  }
+
+  render() {
+    return (
+      <div ref = {this.datePickerContainer}/>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    selectedDate: null,
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello jQueryUI!</h1>
+        <h2>{this.state.selectedDate ? this.state.selectedDate : "Please select date"}</h2>
+        <DatePicker onDateChanged={date => {this.setState({selectedDate: date})}}/>
+      </div>
+    );
+  }
 }
 
 export default App;
